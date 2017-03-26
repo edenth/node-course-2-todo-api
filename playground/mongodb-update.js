2ae00e5d7c27fb329f8d641529ff83bf14aaf598
Parin <parin.nut@gmail.com>
2017-03-26 20:03:16 +0700

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,42 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) => {
+  if(err){
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID("58d4156103554c67648910ce")
+  // },{
+  //   $set: {
+  //     complete: true
+  //   }
+  // },{
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+db.collection('Users').findOneAndUpdate({
+  _id: new ObjectID("58d2b26a48287a25388e6d41")
+},{
+  $inc: {
+    age: 1
+  },
+  $set: {
+    name: 'Nut'
+  }
+},{
+  returnOriginal: false
+}).then((result) => {
+  console.log(result);
+});
+
+
+//db.close();
+});
