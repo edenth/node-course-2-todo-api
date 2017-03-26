61e0a0e91b68bf53b63f7eb1f26128ddf9af3b07
Parin <parin.nut@gmail.com>
2017-03-27 01:40:06 +0700

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.post('/todos',(req,res) => {
 
   todo.save().then((doc) => {
     res.send(doc);
-    console.log(req.body);
+    //console.log(req.body);
   },(e) => {
     res.status(400).send(e);
   });
@@ -25,3 +25,5 @@ app.post('/todos',(req,res) => {
 app.listen(3000, () => {
   console.log('Starting on port 3000');
 });
+
+module.exports = {app}
