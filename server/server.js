6cbd38d122b0be8cf128f40ca55a1626239e82f0
Parin <parin.nut@gmail.com>
2017-03-30 00:27:52 +0700

    Fix header response

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use((req, res, next) => {
           res.setHeader('Access-Control-Allow-Origin', '*');
           res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-          res.setHeader('Access-Control-Allow-Methods", " GET, POST, PUT');
+          res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE");
           next();
       });
 
