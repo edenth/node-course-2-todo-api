4d7e0a0b8198295dbfbe74a0adefab037f92c525
Parin <parin.nut@gmail.com>
2017-03-29 21:36:02 +0700

    Setup separate test and development env

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config');
+
 const _ = require('lodash');
 const express = require('express');
 var bodyParser = require('body-parser');
@@ -8,13 +10,14 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    // title: req.body.title
   });
 
   todo.save().then((doc) => {
@@ -71,6 +74,32 @@ app.delete('/todos/:id',(req,res) => {
 app.patch('/todos/:id',(req,res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text','completed']);
+
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body},{new: true}).then((todo) => {
+    if(!todo){
+      res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.patch('/todos/status/:id',(req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['completed']);
 
   if(!ObjectID.isValid(id)){
     return res.status(404).send();
