1a81fbafdd6cbba36710e6093c118c45355e1e3a
Parin <parin.nut@gmail.com>
2017-03-30 00:18:10 +0700

    add set header response

--- server/server.js
+++ server/server.js
@@ -16,6 +16,8 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
           res.setHeader('Access-Control-Allow-Origin', '*');
+          res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          res.setHeader('Access-Control-Allow-Methods", " GET, POST, PUT');
           next();
       });
 
